0d0acdc0eac652642aeb4394865854c47d90d38b
Javier <jmartin@grupoloyal.com>
2024-12-02 14:34:52 +0100

    Solucionado eslint.config

--- curso/eslint.config.mjs
+++ curso/eslint.config.mjs
@@ -5,6 +5,10 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     rules: {
       "no-unused-vars": "warn",
@@ -12,8 +16,4 @@ export default [
       "@typescript-eslint/no-unused-vars": "off"
     }
   },
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
 ];
