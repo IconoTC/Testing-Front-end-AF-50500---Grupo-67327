4ff3b7176615ed0b95da4e2ff21c32deb88664ec
Javier <jmartin@grupoloyal.com>
2024-12-03 11:55:48 +0100

    casos de prueba

--- curso/spec/demos.spec.js
+++ curso/spec/demos.spec.js
@@ -1,22 +1,57 @@
-const { suma } = require('../src/demos.js')
+const { suma, divide } = require('../src/demos.js')
 
 describe('Ejemplos del curso', () => {
-    describe('Suma', () => {
+    describe('Función Suma', () => {
         describe('OK', () => {
             it('Suma dos entero', () => {
-                const actual = suma(2, 2)
+                const actual = suma(2, 1)
 
-                expect(actual).toBe(4)
+                expect(actual).toBe(3)
             });
             it('Suma dos reales', () => {
-
+                const actual = suma(0.1, 0.2)
+                expect(actual).toBe(0.3)
             });
         });
         describe('KO', () => {
-            it('Suma cualquier cosa', () => {
+            it('Suma cadena numérica con numero', () => {
+                const actual = suma("2", 2)
+
+                expect(actual).toEqual(4)
+            });
+            it('Suma numero con cadena numérica', () => {
                 const actual = suma(2, "2")
 
-                expect(actual).toEqual(22)
+                expect(actual).toEqual(4)
+            });
+            it('Suma un solo numero', () => {
+                expect(() => suma(2)).toThrow("No es un resultado numérico")
+            });
+   
+        });
+            
+    });
+    describe('Función Divide', () => {
+        describe('OK', () => {
+            it('Divide dos entero', () => {
+                const actual = divide(1, 2)
+
+                expect(actual).toBe(0.5)
+            });
+            it('Divide dos entero', () => {
+                const actual = divide(1, 3)
+
+                //expect(actual).toBe(0.3)
+                expect(actual).toBeCloseTo(0.3333, 4)
+            });
+        });
+        describe('KO', () => {
+            it('Divide por 0', () => {
+                expect(() => divide(1, 0)).toThrow("No es un resultado numérico")
+            });
+            it.skip('Divide cadena numérica por numero', () => {
+                expect(divide("kk", 2)).not.toBeNaN()
+                expect(divide(2, "kk")).toBeNaN()
             });
    
         });
--- curso/src/demos.js
+++ curso/src/demos.js
@@ -1,5 +1,16 @@
 function suma(a, b) {
-    return a + b;
+    //if(arguments.length !== suma.caller.arguments.length ) throw new Error("Error en los argumentos")
+    const result = +a + +b
+    if(isNaN(result))
+        throw new Error("No es un resultado numérico valido")
+    return +result.toPrecision(15);
+}
+function divide(a, b) {
+    const result = a / b
+    if(isNaN(result) || !isFinite(result))
+         throw new Error("No es un resultado numérico valido")
+    return +result.toPrecision(15);
 }
 
 module.exports.suma=suma
+module.exports.divide=divide
